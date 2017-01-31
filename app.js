9d6a5036bf9b3fa9106c357648e539255b49cae1
Raoul Rodriguez <raouljrodriguez@gmail.com>
2017-01-31 00:14:49 -0500

    testing image creation over www

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-require("mathmode")("e^{i \\pi} = -1").pipe(process.stdout)
+var express = require('express');
+var app = express();
+var mathmode = require('mathmode');
+
+// we need to enable CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'https://compose.mixmax.com');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Credentials', 'true');
+
+    next();
+});
+
+app.get('/', function(req, res, next) {
+    return self.mathmode("e^{i \\pi} = -1").pipe(process.stdout)
+});
